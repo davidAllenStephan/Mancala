3b48ebda56481d39772ead62055ceaea00bc92ff
davidAllenStephan <david.marino03@hotmail.com>
2022-12-23 17:47:17 -0600

    getting random working

--- board.cpp
+++ board.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <cstdlib>
 #include <ctime>
+#include <random>
 
 #include "displayBoard.hpp"
 #include "determines/determineWinner.hpp"
@@ -14,43 +15,64 @@
 
 using namespace std;
 
-int playerBoard[2][6] = {{4,4,4,4,4,4}, {4,4,4,4,4,4}};
-int playerStore[2] = {0, 0};
-
-// PLAYER 1 VARIABLES
-int player_1_board[6] = {4, 4, 4, 4, 4, 4};
-int player_1_store = 0;
-
-// PLAYER 2 VARIABLESd
-int player_2_board[6] = {4, 4, 4, 4, 4, 4};
-int player_2_store = 0;
-
-int playerTurn = 0;
-int isActive = 1;
-int winner = 0;
-
 int main()
 {
+	int playerOneWins = 0;
+	int playerTwoWins = 0;
+	int draws = 0;
+	int randSeeds[10];
+
 	srand((unsigned)time(NULL));
-	displayBoard(playerBoard, playerStore);
-	while (isActive > 0)
+	
+	for (int i = 0; i < 10; i++)
 	{
-		int move = getRandom(5);
-		// int move = 0;
-		// cin >> move;
-		// move--;
-		playerTurn = progressGame(move, playerTurn, playerBoard, playerStore);
-		isActive = determineSideEmpty(playerBoard[playerTurn]);
+		
+		int isActive = 1;
+		int playerBoard[2][6] = {{4, 4, 4, 4, 4, 4}, {4, 4, 4, 4, 4, 4}};
+		int playerStore[2] = {0, 0};
+		int playerTurn = 0;
+
+		
 		displayBoard(playerBoard, playerStore);
+
+		while (isActive > 0)
+		{
+			int move = getRandom(5);
+			// int move = 0;
+			// cin >> move;
+			// move--;
+			playerTurn = progressGame(move, playerTurn, playerBoard, playerStore);
+			isActive = determineSideEmpty(playerBoard[playerTurn]);
+			displayBoard(playerBoard, playerStore);
+
+			playerStore[0] += getArrayTotal(playerBoard[0], 6);
+			playerStore[1] += getArrayTotal(playerBoard[1], 6);
+			displayBoard(playerBoard, playerStore);
+		}
+
+		int winner = determineWinner(playerStore[0], playerStore[1]);
+
+		if (winner == 0)
+		{
+			playerOneWins++;
+		}
+		else if (winner == 1)
+		{
+			playerTwoWins++;
+		}
+		else if (winner == 3)
+		{
+			draws++;
+		}
 	}
 
-	playerStore[0] += getArrayTotal(playerBoard[0], 6);
-	playerStore[1] += getArrayTotal(playerBoard[1], 6);
-	displayBoard(playerBoard, playerStore);
-	
-	winner = determineWinner(playerStore[0], playerStore[1]);
-
-	cout << winner << "\n";
+	cout << "\n"
+		 << "Player one won: " << playerOneWins;
+	cout << "\n"
+		 << "Player two won: " << playerTwoWins;
+	cout << "\n"
+		 << "Draws: " << draws;
+	cout << "\n";
 
 	return 0;
 }
